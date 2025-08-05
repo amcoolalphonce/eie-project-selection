6344f61971b60031619588811f61ae7a2aae81ba
Alphonce <alphonceomondi273@gmail.com>
2025-08-05 13:27:57 +0300

    CSV

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ProjectCSV(models.Model):
+    file = models.FileField(upload_to='project_csvs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"CSV uploaded at {self.uploaded_at}"
--- base/views.py
+++ base/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+import csv
+from .models import ProjectCSV
+from django.http import HttpResponse
+#messages
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'base/home.html')
+    latest_csv = ProjectCSV.objects.last()
+    
+    if not latest_csv:
+        messages.error(request, "No CSV file uploaded yet.")
+    
+    projects = []
+    
+
+    with latest_csv.file.open('r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            project = {
+                'PRJ_NUMBER': row.get('prj_number', ''),
+                'PRJ_TITLE': row.get('prj_title', ''), 
+    
+            }
+            projects.append(project)
+    
+    return render(request, 'base/results.html', {'projects': projects})
