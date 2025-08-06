6500b0dae5daef707192819613b03e217259474a
ISLOWIsmaeli <Lenojore67@gmail.com>
2025-08-06 14:09:28 +0300

    models new

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import ProjectCSV
+from .models import ProjectCSV, Project
 
 
 admin.site.register(ProjectCSV)
-
+admin.site.register(Project)
--- base/models.py
+++ base/models.py
@@ -10,16 +10,8 @@ class ProjectCSV(models.Model):
         return f"CSV uploaded at {self.uploaded_at}"
 
 class Project(models.Model):
-    prj_number = models.CharField(max_length=20, unique=True)
-    prj_title = models.CharField(max_length=255)
-
+    project_title = models.CharField(max_length=255)
+    project_number = models.TextField()
+    
     def __str__(self):
-        return f"{self.prj_number}: {self.prj_title}"
-
-class ProjectSelection(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('user', 'project')
+        return self.project_title
