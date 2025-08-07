287f04c205cb5494d11f73b3bd19f8a4132a3b3e
makanaraini <danielmakana871@gmail.com>
2025-08-07 15:35:44 +0300

    Removes view titles

--- users/models.py
+++ users/models.py
@@ -1,62 +1,69 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
+    """
+    Custom manager for User model with no username field.
+    """
     def create_user(self, username, email, password=None, **extra_fields):
+        """
+        Create and save a regular User with the given username, email, and password.
+        """
         if not username:
-            raise ValueError('The Username field must be set')
+            raise ValueError('The Username must be set')
         if not email:
-            raise ValueError('The Email field must be set')
-
+            raise ValueError('The Email must be set')
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, username, email, password=None, **extra_fields):
+        """
+        Create and save a SuperUser with the given username, email, and password.
+        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
         return self.create_user(username, email, password, **extra_fields)
-    
-    def get_by_natural_key(self, username):
-        return self.get(username=username)
 
-
-class User(AbstractBaseUser,PermissionsMixin):
+class User(AbstractBaseUser):
+    """
+    Custom User model.
+    """
+    # Identification fields
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(unique=True)
+
+    # Personal info
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
+
+    # Permissions
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)   
+    is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    
-    objects = UserManager()
-    
+
+    # Required fields for Django
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
 
+    # Manager
+    objects = UserManager()
+
     def __str__(self):
         return self.username
-    
-    def natural_key(self):
-        return (self.username,)
-    
+
     def has_perm(self, perm, obj=None):
-        """Does the user have a specific permission?"""
+        """
+        Does the user have a specific permission?
+        """
         return self.is_superuser
-    
+
     def has_module_perms(self, app_label):
-        """Does the user have permissions to view the app `app_label`?"""
+        """
+        Does the user have permissions to view the app `app_label`?
+        """
         return self.is_superuser
 
     class Meta:
